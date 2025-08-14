800487076fbea94b6e7d2c0b0abb4205883d70f3
KongQi52 <kongqi_air@qq.com>
2025-08-14 15:24:05 +0000

    install修改

--- fish/config.fish
+++ fish/config.fish
@@ -30,9 +30,30 @@ function add_sudo
     end
 end
 
+function fuck -d "Correct your previous console command"
+    set -l fucked_up_command $history[1]
+    set -l unfucked_command (env TF_SHELL=fish TF_ALIAS=fuck PYTHONIOENCODING=utf-8 thefuck $fucked_up_command THEFUCK_ARGUMENT_PLACEHOLDER $argv)
+    if test -n "$unfucked_command"
+        eval $unfucked_command
+        builtin history delete --exact --case-sensitive -- $fucked_up_command
+        builtin history merge
+    end
+end
+
+function FUCK -d "Correct your previous console command"
+    set -l fucked_up_command $history[1]
+    set -l unfucked_command (env TF_SHELL=fish TF_ALIAS=FUCK PYTHONIOENCODING=utf-8 thefuck $fucked_up_command THEFUCK_ARGUMENT_PLACEHOLDER $argv)
+    if test -n "$unfucked_command"
+        eval $unfucked_command
+        builtin history delete --exact --case-sensitive -- $fucked_up_command
+        builtin history merge
+    end
+end
+
 # 绑定Ctrl+S添加sudo（因为Ctrl+S通常不被使用）
 bind \cs add_sudo
 # 设置Starship配置文件路径
 set -gx STARSHIP_CONFIG /opt/my-meihua/starship/starship.toml
 
-eval (ssh-agent -c)
+eval (ssh-agent -c) >> /dev/null 2>&1 &
+alias cnpm="npm --registry=https://registry.npmmirror.com  --cache=$HOME/.npm/.cache/cnpm  --disturl=https://npmmirror.com/mirrors/node  --userconfig=$HOME/.cnpmrc"
